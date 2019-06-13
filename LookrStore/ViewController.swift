b59677f1a9f17cbbd36d60f06ceb29e0fbaeccf7
Aditya Mohan <adityamohan@Adityas-MacBook-Air.local>
2019-06-13 17:50:53 +0530

    login screen have added

--- LookrStore/ViewController.swift
+++ LookrStore/ViewController.swift
@@ -14,10 +14,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var btnLogin: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        btnLogin.layer.cornerRadius = 4
-
         // Do any additional setup after loading the view.
         
         //self.topImageView.image = UIImage.gif(name: "jeremy")
@@ -29,7 +28,128 @@ class ViewController: UIViewController {
 
     }
     
-    
-
 }
+
+
+@IBDesignable class MyButton: UITextField
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0{
+        
+        didSet{
+            
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        
+        didSet {
+            
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        
+        super.prepareForInterfaceBuilder()
+    }
+    
+    func updateCornerRadius(radius:CGFloat) {
+        layer.cornerRadius = radius
+    }
+    @IBInspectable var cornerRadius:CGFloat = 0{
+        didSet{
+            updateCornerRadius(radius: cornerRadius)
+        }
+    }
+}
+
+
+@IBDesignable
+class RoundedCornerView: UIButton {
+    
+    var cornerRadiusValue : CGFloat = 0
+    var corners : UIRectCorner = []
+    
+    @IBInspectable public var cornerRadius:CGFloat {
+        get {
+            return cornerRadiusValue
+        }
+        set {
+            cornerRadiusValue = newValue
+        }
+    }
+    
+    @IBInspectable public var topLeft : Bool {
+        get {
+            return corners.contains(.topLeft)
+        }
+        set {
+            setCorner(newValue: newValue, for: .topLeft)
+        }
+    }
+    
+    @IBInspectable public var topRight : Bool {
+        get {
+            return corners.contains(.topRight)
+        }
+        set {
+            setCorner(newValue: newValue, for: .topRight)
+        }
+    }
+    
+    @IBInspectable public var bottomLeft : Bool {
+        get {
+            return corners.contains(.bottomLeft)
+        }
+        set {
+            setCorner(newValue: newValue, for: .bottomLeft)
+        }
+    }
+    
+    @IBInspectable public var bottomRight : Bool {
+        get {
+            return corners.contains(.bottomRight)
+        }
+        set {
+            setCorner(newValue: newValue, for: .bottomRight)
+        }
+    }
+    
+    func setCorner(newValue: Bool, for corner: UIRectCorner) {
+        if newValue {
+            addRectCorner(corner: corner)
+        } else {
+            removeRectCorner(corner: corner)
+        }
+    }
+    
+    func addRectCorner(corner: UIRectCorner) {
+        corners.insert(corner)
+        updateCorners()
+    }
+    
+    func removeRectCorner(corner: UIRectCorner) {
+        if corners.contains(corner) {
+            corners.remove(corner)
+            updateCorners()
+        }
+    }
+    
+    func updateCorners() {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadiusValue, height: cornerRadiusValue))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+}
+
 
